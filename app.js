7764836020e6a9a9a948c2e9a7639415e2aef69d
nikhilstacks <nikhilstacks@gmail.com>
2022-01-11 22:51:31 +0530

    Mongoose addition

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -11,6 +12,19 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/blogDB");
+
+const postSchema = {
+  name: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+const post = new Post({
+  name: "Nikhil"
+});
+
 
 app.set('view engine', 'ejs');
 
